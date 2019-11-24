4740fc84ac3ba428c7ada3b18da87326044eafd4
ZumbaIdol <david.a.foote@gmail.com>
2019-11-23 20:04:10 -0600

    Added associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  belongs_to :user
+  has_many :comments through :users
+  validate :title, :category, :rating, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :movies
+  has_many :comments
+  validates :name, :email, presence: true
 end
