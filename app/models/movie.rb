d0c8bef7ba84e5437257dda06697b9f0a085f22e
ZumbaIdol <david.a.foote@gmail.com>
2019-12-02 20:30:14 -0600

    Changed has_many relationship to movie_log in the models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :user
-  has_many :comments, through: :users
+  has_many :comments, through: :movie_log
   validates :title, :category, :rating, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :movies
-  has_many :comments, through: :movies
+  has_many :comments, through: :movie_log
   validates :name, :email, presence: true, uniqueness: true
 end
