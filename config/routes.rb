d3dfed338e92992804a9178b1cd0067ac7b35cb0
ZumbaIdol <david.a.foote@gmail.com>
2019-12-05 22:56:10 -0600

    Changed routing root, still working on user being saved at login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'sessions#new'
-  get '/auth/facebook/callback' => 'sessions#create'
+  root 'welcome#home'
+  get '/auth/facebook/callback', to: 'welcome#home'
   get '/signup', to: 'users#new', as: 'signup'
-  get '/login' => 'sessions#new'
+  get '/login', to: 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
