46559d507e1e150d00e4a8f75189a4ea350dc0f3
ZumbaIdol <david.a.foote@gmail.com>
2019-12-06 21:22:03 -0600

    App was broken, removed Omniauth temporarily, working again, mostly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
-  get '/auth/facebook/callback', to: 'welcome#home'
+  root 'sessions#new'
+  get '/auth/facebook/callback' => 'sessions#create'
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
