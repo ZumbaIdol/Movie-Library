c7cf967aa650fda1289a097ac9fc285c669c3101
ZumbaIdol <david.a.foote@gmail.com>
2019-12-11 09:54:26 -0600

    Added spec.md file, making progress with regular login, need to fix form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
     @user = User.from_omniauth(auth)
   # end
   session[:user_id] = @user.id
-  redirect_to users_path(@user)
+  redirect_to user_path(@user)
 end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   def self.from_omniauth(auth)
-    # where(email: auth.info.email).first_or_initialize do |u|
-    #   u.name = auth.info.name
-    #   u.email = auth.info.email
-    #   u.password = SecureRandom.hex
     self.where(email: auth["info"]["email"]).first_or_create do |u|
       u.name = auth["info"]["name"]
       u.password = SecureRandom.hex
