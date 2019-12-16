35175fde63b1f423ddffe66b88d62992bea00f62
ZumbaIdol <david.a.foote@gmail.com>
2019-12-16 10:02:32 -0600

    Corrected association error in Movie model (:movie_logs)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :movie_logs
-  has_many :users, through: :movie_log
+  has_many :users, through: :movie_logs
   validates :title, :category, :rating, presence: true
   # scope :long_titles, ->(n) { where("LENGTH(title) > ?", n) }
   # scope :rating, ->(r) { where(rating: 'r') }
