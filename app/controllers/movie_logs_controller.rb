4202de267a0ed4138dd57bb81f820f82d6fb4b44
ZumbaIdol <david.a.foote@gmail.com>
2019-12-27 10:01:14 -0600

    Modified movie_logs_controller#destroy to redirect to movies_path(current_user) instead of user_path; '.' in url instead of '/'.

--- app/controllers/movie_logs_controller.rb
+++ app/controllers/movie_logs_controller.rb
@@ -52,7 +52,7 @@ class MovieLogsController < ApplicationController
   def destroy
     MovieLog.find(params[:id]).destroy
     flash[:notice] = "Comment was successfully deleted!"
-    redirect_to user_path(current_user)
+    redirect_to movies_path(current_user)
   end
 
 
